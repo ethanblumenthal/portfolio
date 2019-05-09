dff14de225495b9896447f44b0338e6ef047097a
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-09 10:36:25 -0500

    added styles to header and layout using css modules

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import layoutStyles from './layout.module.scss'
 import '../styles/index.scss'
 
 const Layout = props => {
   return (
-    <div>
-      <Header />
-      {props.children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {props.children}
+      </div>
       <Footer />
     </div>
   )
