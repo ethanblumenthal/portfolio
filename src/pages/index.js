1056edd304a385f34d5a843fd3948afe35e4e71e
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-09 09:35:27 -0500

    created pages for index, blog, about, and contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <h2>I'm Ethan, a full stack developer living in Chicago.</h2>
+    </div>
+  )
+}
+
+export default Index
