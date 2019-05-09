1995653a5fdb6a6a8c3c5ec4439d18c99775527d
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-09 10:18:40 -0500

    added sass styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import '../styles/index.scss'
 
 const Layout = props => {
   return (
