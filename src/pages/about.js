dda9176965117f18339eaf452468a05e0a3f7be4
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-13 10:49:51 -0500

    styled about page and added photos

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,39 @@ const About = () => {
   return (
     <Layout>
       <Head title="About" />
-      <h1>About</h1>
-      <p>I am a full stack software developer specializing in JavaScript (React and Node) currently seeking a new opportunity. I previously worked in marketing roles in the music and education industries connecting consumers with unique experiences. However, I have always been fascinated by the endless potential for technology and realized that I wanted to build the experiences themselves. Learning to code at Fullstack Academy has launched my journey and I now aim to utilize these skills to contribute to an innovative company improving the world. I look forward to connecting with you soon!</p>
+      <section className="section-about">
+        <div className="row">
+          <div className="col-1-of-2">
+            <h3 className="heading-tertiary u-margin-bottom-small">Hello!</h3>
+            <p className="paragraph">
+            I am a full stack software developer specializing in JavaScript (React and Node) currently seeking a new opportunity. I previously worked in marketing roles in the music and education industries connecting consumers with unique experiences. However, I have always been fascinated by the endless potential for technology and realized that I wanted to build the experiences themselves. Learning to code at Fullstack Academy has launched my journey and I now aim to utilize these skills to contribute to an innovative company improving the world. I look forward to connecting with you soon!
+            </p>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ethanblumenthal/" className="btn-text">Learn more &rarr;</a>
+          </div>
+          <div className="col-1-of-2">
+            <div className="composition">
+              <img srcSet="img/ethan-1.jpg 300w"
+                sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
+                alt="Profile Picture"
+                className="composition__photo composition__photo--p1"
+                src="img/ethan-1.jpg" />
+              <img srcSet="img/ethan-2.jpg 300w"
+                sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
+                alt="Golden Gate Bridge"
+                className="composition__photo composition__photo--p2"
+                src="img/ethan-2.jpg" />
+              <img srcSet="img/ethan-2.jpg 300w"
+                sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
+                alt="Lennon Wall"
+                className="composition__photo composition__photo--p3"
+                src="img/ethan-2.jpg" />
+              <img src="img/ethan-1.jpg" alt="Profile Picture" className="composition__photo composition__photo--p1" />
+              <img src="img/ethan-2.jpg" alt="Golden Gate Bridge" className="composition__photo composition__photo--p2" />
+              <img src="img/ethan-3.jpg" alt="Lennon Wall" className="composition__photo composition__photo--p3" />
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,114 +7,105 @@ const Projects = () => {
     <Layout>
       <Head title="Projects" />
       <section className="section-tours" id="section-tours">
-          <div className="u-center-text u-margin-bottom-big">
-              <h2 className="heading-secondary">
-                  Projects
-              </h2>
-          </div>
-
-          <div className="row">
-              <div className="col-1-of-3">
-                  <div className="card">
-                      <div className="card__side card__side--front">
-                          <div className="card__picture card__picture--1">
-                              &nbsp;
-                          </div>
-                          <h4 className="card__heading">
-                              <span className="card__heading-span card__heading-span--1">Hype Swipe</span>
-                          </h4>
-                          <div className="card__details">
-                              <ul>
-                                  <li>React Native & Firebase</li>
-                                  <li>CSS Animations</li>
-                                  <li>Push Notifications</li>
-                                  <li>Twilio One Time Passwords</li>
-                                  <li>AWS Serverless Computing</li>
-                              </ul>
-                          </div>
-                      </div>
-                      <div className="card__side card__side--back card__side--back-1">
-                          <div className="card__cta">
-                              <div className="card__price-box">
-                                  <p className="card__price-value">Hype Swipe</p>
-                                  <p className="card__price-only">Find new places to expore around you!</p>
-                              </div>
-                              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ethanblumenthal/hype-swipe" className="btn btn--white">View Code</a>
-                          </div>
-                      </div>
-                  </div>
+        <div className="row">
+          <div className="col-1-of-3">
+            <div className="card">
+              <div className="card__side card__side--front">
+                <div className="card__picture card__picture--1">
+                  &nbsp;
+                </div>
+                <h4 className="card__heading">
+                  <span className="card__heading-span card__heading-span--1">Hype Swipe</span>
+                </h4>
+                <div className="card__details">
+                  <ul>
+                    <li>React Native & Firebase</li>
+                    <li>CSS Animations</li>
+                    <li>Push Notifications</li>
+                    <li>Twilio One Time Passwords</li>
+                    <li>AWS Serverless Computing</li>
+                  </ul>
+                </div>
               </div>
-
-              <div className="col-1-of-3">
-                  <div className="card">
-                      <div className="card__side card__side--front">
-                          <div className="card__picture card__picture--2">
-                              &nbsp;
-                          </div>
-                          <h4 className="card__heading">
-                              <span className="card__heading-span card__heading-span--2">Product Loop</span>
-                          </h4>
-                          <div className="card__details">
-                              <ul>
-                                  <li>Node / Express / MongoDB</li>
-                                  <li>Payments with Stripe</li>
-                                  <li>Webhooks with Sendgrid</li>
-                                  <li>Data Caching with Redis</li>
-                                  <li>Automated Testing with Jest</li>
-                              </ul>
-                          </div>
-
+              <div className="card__side card__side--back card__side--back-1">
+                  <div className="card__cta">
+                      <div className="card__price-box">
+                          <p className="card__price-value">Hype Swipe</p>
+                          <p className="card__price-only">Find new places to expore around you!</p>
                       </div>
-                      <div className="card__side card__side--back card__side--back-2">
-                          <div className="card__cta">
-                              <div className="card__price-box">
-                                  <p className="card__price-value">Product Loop</p>
-                                  <p className="card__price-only">Get insightful email feedback on your product!</p>
-                              </div>
-                              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ethanblumenthal/product-loop" className="btn btn--white">View Code</a>
-                              <a target="_blank" rel="noopener noreferrer" href="https://product-loop.herokuapp.com/" className="btn btn--white">Demo App</a>
-                          </div>
-                      </div>
+                      <a target="_blank" rel="noopener noreferrer" href="https://github.com/ethanblumenthal/hype-swipe" className="btn btn--white">View Code</a>
                   </div>
+                </div>
               </div>
+            </div>
 
-              <div className="col-1-of-3">
-                  <div className="card">
-                      <div className="card__side card__side--front">
-                          <div className="card__picture card__picture--3">
-                              &nbsp;
-                          </div>
-                          <h4 className="card__heading">
-                              <span className="card__heading-span card__heading-span--3">Code Along</span>
-                          </h4>
-                          <div className="card__details">
-                              <ul>
-                                  <li>React / GraphQL / PostgreSQL</li>
-                                  <li>Realtime Messaging Protocol</li>
-                                  <li>Google & Facebook Oauth</li>
-                                  <li>Higher Order Components</li>
-                                  <li>Middlewares & Hooks</li>
-                              </ul>
-                          </div>
-
-                      </div>
-                      <div className="card__side card__side--back card__side--back-3">
-                          <div className="card__cta">
-                              <div className="card__price-box">
-                                  <p className="card__price-value">Code Along</p>
-                                  <p className="card__price-only">Live stream your coding sessions to followers!</p>
-                              </div>
-                              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ethanblumenthal/code-along" className="btn btn--white">View Code</a>
-                              <a target="_blank" rel="noopener noreferrer" href="https://code-along-1.herokuapp.com/" className="btn btn--white">Demo App</a>
-                          </div>
-                      </div>
+            <div className="col-1-of-3">
+              <div className="card">
+                <div className="card__side card__side--front">
+                  <div className="card__picture card__picture--2">
+                    &nbsp;
                   </div>
+                  <h4 className="card__heading">
+                    <span className="card__heading-span card__heading-span--2">Product Loop</span>
+                  </h4>
+                  <div className="card__details">
+                    <ul>
+                      <li>Node / Express / MongoDB</li>
+                      <li>Payments with Stripe</li>
+                      <li>Webhooks with Sendgrid</li>
+                      <li>Data Caching with Redis</li>
+                      <li>Automated Testing with Jest</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card__side card__side--back card__side--back-2">
+                  <div className="card__cta">
+                    <div className="card__price-box">
+                      <p className="card__price-value">Product Loop</p>
+                      <p className="card__price-only">Get insightful email feedback on your product!</p>
+                    </div>
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/ethanblumenthal/product-loop" className="btn btn--white">View Code</a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://product-loop.herokuapp.com/" className="btn btn--white">Demo App</a>
+                  </div>
+                </div>
               </div>
-          </div>
+            </div>
 
-          <div className="u-center-text u-margin-top-huge">
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ethanblumenthal" className="btn btn--green">View all Projects</a>
+            <div className="col-1-of-3">
+              <div className="card">
+                <div className="card__side card__side--front">
+                  <div className="card__picture card__picture--3">
+                    &nbsp;
+                  </div>
+                  <h4 className="card__heading">
+                    <span className="card__heading-span card__heading-span--3">Code Along</span>
+                  </h4>
+                  <div className="card__details">
+                    <ul>
+                      <li>React / GraphQL / PostgreSQL</li>
+                      <li>Realtime Messaging Protocol</li>
+                      <li>Google & Facebook Oauth</li>
+                      <li>Higher Order Components</li>
+                      <li>Middlewares & Hooks</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card__side card__side--back card__side--back-3">
+                  <div className="card__cta">
+                    <div className="card__price-box">
+                      <p className="card__price-value">Code Along</p>
+                      <p className="card__price-only">Live stream your coding sessions to followers!</p>
+                    </div>
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/ethanblumenthal/code-along" className="btn btn--white">View Code</a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://code-along-1.herokuapp.com/" className="btn btn--white">Demo App</a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+        <div className="u-center-text u-margin-top-huge">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/ethanblumenthal" className="btn btn--green">View all &rarr;</a>
+        </div>
       </section>
     </Layout>
   )
