ecda821b6173eb21b523b7a1f8f9c74d8adbe47a
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-16 15:29:25 -0500

    added typescript blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,15 +6,15 @@ const Blog = () => {
     <Layout color="#777" title="Blog">
       <section className="section-stories">
         <div className="row">
-          <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ethanblumenthal/hello-world-graphql-edition-dc4bf1a59cc" className="story__link">
+          <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ethanblumenthal/level-up-your-web-development-skills-with-typescript-b8f70da8712" className="story__link">
             <div className="story">
               <figure className="story__shape">
-                <img src="img/nat-6.jpg" alt="GraphQL" className="story__img" />
-                <figcaption className="story__caption">GraphQL</figcaption>
+                <img src="img/nat-6.jpg" alt="TypeScript" className="story__img" />
+                <figcaption className="story__caption">TypeScript</figcaption>
               </figure>
               <div className="story__text">
-                <h3 className="heading-tertiary u-margin-bottom-small">Hello World — GraphQL Edition</h3>
-                <p>GraphQL is the new kid on the block when it comes to API design. If you’re looking to improve the flow of data in your applications, then the time is right to explore this exciting new technology. GraphQL allows users to declaratively fetch data by specifying exactly what they want. As applications have become increasingly complex, data has become heavily nested, creating headaches for developers...</p>
+                <h3 className="heading-tertiary u-margin-bottom-small">Level Up Your Web Development Skills With TypeScript</h3>
+                <p>Want to level up your web development skills? Then TypeScript just might be the answer. TypeScript is an open source programming language that is developed and maintained by Microsoft. You can think of TypeScript as a wrapper around JavaScript, built on top of ES6, that gives you extended functionality. However, TypeScript must still be compiled to JavaScript in order to run in the browser...</p>
               </div>
             </div>
           </a>
@@ -22,14 +22,14 @@ const Blog = () => {
 
         <div className="row">
           <div className="story">
-            <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ethanblumenthal/technologies-shaping-the-future-3df36c61761e" className="story__link">
+            <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ethanblumenthal/hello-world-graphql-edition-dc4bf1a59cc" className="story__link">
               <figure className="story__shape">
-                <img src="img/nat-7.jpg" alt="Future" className="story__img" />
-                <figcaption className="story__caption">Future</figcaption>
+                <img src="img/nat-7.jpg" alt="GraphQL" className="story__img" />
+                <figcaption className="story__caption">GraphQL</figcaption>
               </figure>
               <div className="story__text">
-                <h3 className="heading-tertiary u-margin-bottom-small">Technologies Shaping the Future</h3>
-                <p>We are currently in an age of lightning fast innovation. Since the dawn of the Internet, new technologies have progressed at a pace never before seen in human history. It is an amazing time to partake in building the future. Below are the top five technologies (in no particular order) that I believe are about to change the world....</p>
+                <h3 className="heading-tertiary u-margin-bottom-small">Build next generation APIs with GraphQL</h3>
+                <p>GraphQL is the new kid on the block when it comes to API design. If you’re looking to improve the flow of data in your applications, then the time is right to explore this exciting new technology. GraphQL allows users to declaratively fetch data by specifying exactly what they want. As applications have become increasingly complex, data has become heavily nested, creating headaches for developers...</p>
               </div>
             </a>
           </div>
